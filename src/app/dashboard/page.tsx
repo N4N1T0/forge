795209814836859a048a5a094f49682efd5682d5
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-07-29 14:47:25 +0200

    refactor(dashboard): remove auth check from dashboard page
    The auth check and redirect logic was moved to a layout component to handle authentication at a higher level, making the dashboard page simpler and more focused on its main purpose.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,6 @@
-import { getCurrent } from '@/features/auth/actions'
 import { UserBtn } from '@/features/auth/components/user-btn'
-import { redirect } from 'next/navigation'
 
 export default async function HomePage() {
-  // STATE
-  const { data: user } = await getCurrent()
-
-  // REDIRECT
-  if (!user) redirect('/')
-
   return (
     <div>
       <UserBtn />
