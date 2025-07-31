c4b3411f0154087f8428cfe1db087fe0b9d0ba14
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-07-31 23:21:17 +0200

    fix(dashboard): adjust layout styles to prevent scroll issues
    Fix layout overflow and center main content by adjusting height classes and adding overflow control. Center children elements in main container for better visual presentation.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from '@/components/layout/sidebar'
 
 export default function DashboardLayout({ children }: Layouts) {
   return (
-    <div className='min-h-screen size-full'>
+    <div className='h-screen size-full overflow-y-hidden'>
       <div className='flex size-full'>
         <div className='fixed left-0 top-0 hidden lg:block lg:w-64 overflow-y-auto h-full'>
           <Sidebar />
@@ -11,7 +11,9 @@ export default function DashboardLayout({ children }: Layouts) {
         <div className='lg:pl-64 w-full'>
           <div className='mx-auto max-w-screen-2xl h-full'>
             <Navbar />
-            <main className='h-full py-8 px-6 flex flex-col'>{children}</main>
+            <main className='size-full py-8 px-6 flex flex-col justify-center items-center'>
+              {children}
+            </main>
           </div>
         </div>
       </div>
