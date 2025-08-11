37d9843c6c4b9892a6aff007c38e7793e721a334
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-08-11 18:46:50 +0200

    docs(utils): add jsdoc for cn function
    Add JSDoc documentation to clarify the purpose and usage of the cn utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
+/**
+ * Combines multiple class values into a single className string using clsx and tailwind-merge
+ * @param inputs - Array of class values to be combined
+ * @returns Merged and deduplicated className string
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
