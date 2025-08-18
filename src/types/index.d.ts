d1622038f5d93afb87ae5cef4a6cbc5a087f8801
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-08-18 17:39:31 +0200

    feat(types): add MiddleWareContext type definition for middleware variables

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,3 +6,13 @@ interface WorkspaceAvatarProps {
   workspace: Workspaces
   className?: string
 }
+
+type MiddleWareContext = {
+  Variables: {
+    account: AccountType
+    databases: DatabasesType
+    storage: StorageType
+    users: Models.User
+    user: Models.User<Models.Preferences>
+  }
+}
