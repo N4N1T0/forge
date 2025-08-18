08a9e64efe19db897825a3d407f68d847f76aae6
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-08-18 17:39:17 +0200

    feat(workspaces): add action to fetch user workspaces from appwrite
    Implement getWorkspaces function to retrieve workspaces associated with the authenticated user. The function handles session validation, fetches user memberships, and returns corresponding workspaces with proper error handling.

---
+++ src/features/workspaces/actions/index.ts
@@ -0,0 +1,65 @@
+import {
+  APPWRITE_ENDPOINT,
+  APPWRITE_PROJECT_ID,
+  DATABASE_ID,
+  MEMBERS_COLLECTION_ID,
+  WORKSPACES_COLLECTION_ID
+} from '@/config'
+import { AUTH_COOKIE } from '@/features/auth/constants'
+import { Members, Workspaces } from '@/types/appwrite'
+import { cookies } from 'next/headers'
+import { Account, Client, Databases, Query } from 'node-appwrite'
+
+export const getWorkspaces = async () => {
+  try {
+    const client = new Client()
+      .setEndpoint(APPWRITE_ENDPOINT)
+      .setProject(APPWRITE_PROJECT_ID)
+
+    const cookie = await cookies()
+    const session = cookie.get(AUTH_COOKIE)
+
+    if (!session)
+      return {
+        success: false,
+        data: null
+      }
+
+    client.setSession(session.value)
+    const databases = new Databases(client)
+    const account = new Account(client)
+    const user = await account.get()
+
+    const members = await databases.listDocuments<Members>(
+      DATABASE_ID,
+      MEMBERS_COLLECTION_ID,
+      [Query.equal('userId', user.$id)]
+    )
+
+    if (members.total === 0) {
+      return {
+        success: true,
+        data: []
+      }
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId)
+
+    const workspaces = await databases.listDocuments<Workspaces>(
+      DATABASE_ID,
+      WORKSPACES_COLLECTION_ID,
+      [Query.orderDesc('$createdAt'), Query.contains('$id', workspaceIds)]
+    )
+
+    return {
+      success: true,
+      data: workspaces.documents
+    }
+  } catch (error) {
+    console.log('🚀 ~ getWorkspaces ~ error:', error)
+    return {
+      success: false,
+      data: null
+    }
+  }
+}
