f0f636aaa873c95849243a9b0f6485e8cf22d6a3
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-08-26 14:32:59 +0200

    refactor(auth): replace direct Appwrite client setup with CreateSessionClient
    Simplify authentication logic by using centralized CreateSessionClient utility instead of manual client configuration and session handling

--- src/features/auth/actions/index.ts
+++ src/features/auth/actions/index.ts
@@ -1,27 +1,11 @@
 import 'server-only'
 
-import { APPWRITE_ENDPOINT, APPWRITE_PROJECT_ID } from '@/config'
-import { AUTH_COOKIE } from '@/features/auth/constants'
-import { cookies } from 'next/headers'
-import { Account, Client } from 'node-appwrite'
+import { CreateSessionClient } from '@/lib/appwrite'
 
 export const getCurrentAction = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(APPWRITE_ENDPOINT)
-      .setProject(APPWRITE_PROJECT_ID)
+    const { account } = await CreateSessionClient()
 
-    const cookie = await cookies()
-    const session = cookie.get(AUTH_COOKIE)
-
-    if (!session)
-      return {
-        success: false,
-        data: null
-      }
-
-    client.setSession(session.value)
-    const account = new Account(client)
     const user = await account.get()
 
     return {
