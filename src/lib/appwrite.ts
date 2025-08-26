f3d6ab922ee5a0be70415a613cd9497adba70e17
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-08-26 14:30:14 +0200

    feat(auth): add session client creation for appwrite
    Add CreateSessionClient function to handle authenticated client sessions using cookies. Also update environment variable names for consistency.

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,11 +1,38 @@
 import 'server-only'
 
-import { Account, Client } from 'node-appwrite'
+import { APPWRITE_ENDPOINT, APPWRITE_PROJECT_ID } from '@/config'
+import { AUTH_COOKIE } from '@/features/auth/constants'
+import { cookies } from 'next/headers'
+import { Account, Client, Databases } from 'node-appwrite'
+
+export async function CreateSessionClient() {
+  const client = new Client()
+    .setEndpoint(APPWRITE_ENDPOINT)
+    .setProject(APPWRITE_PROJECT_ID)
+
+  const cookie = await cookies()
+  const session = cookie.get(AUTH_COOKIE)
+
+  if (!session || !session.value) {
+    throw new Error('No session found')
+  }
+
+  client.setSession(session.value)
+
+  return {
+    get account() {
+      return new Account(client)
+    },
+    get databases() {
+      return new Databases(client)
+    }
+  }
+}
 
 export async function createAdminClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
     .setKey(process.env.NEXT_APPWRITE_KEY!)
 
   return {
