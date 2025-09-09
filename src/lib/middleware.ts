622fc5320348ad43eb3f7ed115b7754493cfa8e7
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-09-09 16:30:31 +0200

    feat(auth): add session and admin middleware for appwrite integration
    Implement middleware to handle user sessions and admin access using Appwrite SDK. The session middleware validates authentication cookies and sets up client resources, while the admin middleware provides admin-level access to Appwrite services.

---
+++ src/lib/middleware.ts
@@ -0,0 +1,67 @@
+import 'server-only'
+
+import {
+  APPWRITE_API_KEY,
+  APPWRITE_ENDPOINT,
+  APPWRITE_PROJECT_ID
+} from '@/config'
+import { AUTH_COOKIE } from '@/features/auth/constants'
+import { AdminMiddleWareContext, MiddleWareContext } from '@/types/functions'
+import { getCookie } from 'hono/cookie'
+import { createMiddleware } from 'hono/factory'
+import { Account, Client, Storage, TablesDB, Users } from 'node-appwrite'
+
+export const sessionMiddleware = createMiddleware<MiddleWareContext>(
+  async (c, next) => {
+    const client = new Client()
+      .setEndpoint(APPWRITE_ENDPOINT)
+      .setProject(APPWRITE_PROJECT_ID)
+
+    const session = getCookie(c, AUTH_COOKIE)
+
+    if (!session) {
+      return c.json(
+        {
+          success: false,
+          message: 'Unauthorized'
+        },
+        401
+      )
+    }
+
+    client.setSession(session)
+
+    const account = new Account(client)
+    const tables = new TablesDB(client)
+    const storage = new Storage(client)
+    const user = await account?.get()
+
+    c.set('account', account)
+    c.set('tables', tables)
+    c.set('storage', storage)
+    c.set('user', user)
+
+    await next()
+  }
+)
+
+export const adminMiddleware = createMiddleware<AdminMiddleWareContext>(
+  async (c, next) => {
+    const client = new Client()
+      .setEndpoint(APPWRITE_ENDPOINT)
+      .setProject(APPWRITE_PROJECT_ID)
+      .setKey(APPWRITE_API_KEY)
+
+    const account = new Account(client)
+    const tables = new TablesDB(client)
+    const storage = new Storage(client)
+    const users = new Users(client)
+
+    c.set('account', account)
+    c.set('tables', tables)
+    c.set('storage', storage)
+    c.set('users', users)
+
+    await next()
+  }
+)
