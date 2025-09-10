cfa52df27026dd1a6e06d5ffa0ecd37c0b1eac3b
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-09-10 17:06:59 +0200

    feat(layout): wrap dashboard layout with theme provider
    Add SpacemanThemeProviderClient to support theme switching functionality

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,24 @@
 import { Navbar } from '@/components/layout/navbar'
 import { AppSidebar } from '@/components/layout/sidebar/app-sidebar'
+import { SpacemanThemeProviderClient } from '@/components/layout/spaceman-theme-provider'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { Layouts } from '@/types'
 
 export default function DashboardLayout({ children }: Layouts) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <Navbar />
-        <main className='flex flex-col size-full justify-center items-center overflow-y-auto py-4'>
-          {children}
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
+    <SpacemanThemeProviderClient
+      themes={['light', 'dark', 'system']}
+      defaultTheme='system'
+    >
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <Navbar />
+          <main className='flex flex-col size-full justify-center items-center overflow-y-auto py-4'>
+            {children}
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </SpacemanThemeProviderClient>
   )
 }
