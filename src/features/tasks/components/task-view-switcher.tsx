11974024c35e2c44604da7a2df503ce063a9db97
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-09-19 16:46:53 +0200

    feat(tasks): add table view component and client directive
    - Add 'use client' directive for client-side rendering
    - Replace placeholder content with TaskTableView component
    - Adjust padding and spacing in the layout

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,11 +1,13 @@
+'use client'
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { TaskTableView } from './views'
 
 export const TaskViewSwitcher = () => {
   return (
-    <Tabs className='flex-1 w-full border'>
-      <div className='h-full flex flex-col overflow-auto py-2 px-4'>
-        <div className='flex flex-col gap-y-2 lg:flex-row justify-between'></div>
-        <TabsList className='w-full lg:w-auto bg-transparent border-b h-12'>
+    <Tabs className='flex-1 w-full border' defaultValue='table'>
+      <div className='h-full flex flex-col overflow-auto p-2'>
+        <TabsList className='w-full lg:w-auto bg-transparent border-b h-12 pb-2'>
           <TabsTrigger value='table' className='w-full lg:w-auto'>
             Table
           </TabsTrigger>
@@ -20,10 +22,8 @@ export const TaskViewSwitcher = () => {
           </TabsTrigger>
         </TabsList>
 
-        {/* FILTERS */}
-
         <TabsContent value='table' className='mt-0'>
-          Data Table
+          <TaskTableView />
         </TabsContent>
         <TabsContent value='kanban' className='mt-0'>
           Kanban
