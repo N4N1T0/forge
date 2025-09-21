99d83cb3a85b6b02a9b57a7670687df6ad12eea0
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-09-21 21:54:51 +0200

    feat(tasks): add task view switcher component with table, kanban, calendar and gantt views
    The component allows users to switch between different task views while maintaining filter state. It uses tabs for navigation and currently implements the table view with placeholder content for other views.

---
+++ src/features/tasks/components/views/task-view-switcher.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { useProjectParams } from '@/features/projects/client/use-project-id'
+import { useTaskFilters } from '@/features/tasks/hooks/use-task-filters'
+import { useGetTasks } from '@/features/tasks/server/use-get-tasks'
+import { useQueryState } from 'nuqs'
+import { TaskTableView } from '.'
+
+export const TaskViewSwitcher = () => {
+  // STATE
+  const [view, setView] = useQueryState('view', {
+    defaultValue: 'table'
+  })
+  const [{ assigneeId, dueDate, search, status }] = useTaskFilters()
+
+  // HOOKS
+  const { workspaceId, projectId } = useProjectParams()
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+    projectId,
+    assigneeId: assigneeId || undefined,
+    dueDate: dueDate || undefined,
+    search: search || undefined,
+    status: status || undefined
+  })
+
+  // RENDER
+  return (
+    <Tabs
+      className='flex-1 w-full border'
+      defaultValue={view}
+      onValueChange={setView}
+    >
+      <div className='h-full flex flex-col overflow-auto p-2'>
+        <TabsList className='w-full lg:w-auto bg-transparent border-b h-12 pb-2'>
+          <TabsTrigger value='table' className='w-full lg:w-auto'>
+            Table
+          </TabsTrigger>
+          <TabsTrigger value='kanban' className='w-full lg:w-auto'>
+            Kanban
+          </TabsTrigger>
+          <TabsTrigger value='calendar' className='w-full lg:w-auto'>
+            Calendar
+          </TabsTrigger>
+          <TabsTrigger value='gantt' className='w-full lg:w-auto'>
+            Gantt
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value='table' className='mt-0'>
+          <TaskTableView data={tasks} isLoading={isLoadingTasks} />
+        </TabsContent>
+        <TabsContent value='kanban' className='mt-0'>
+          Kanban
+        </TabsContent>
+        <TabsContent value='calendar' className='mt-0'>
+          Calendar
+        </TabsContent>
+        <TabsContent value='gantt' className='mt-0'>
+          Gantt
+        </TabsContent>
+      </div>
+    </Tabs>
+  )
+}
