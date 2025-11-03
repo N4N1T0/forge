b6054cb59ce07066e8512ef4e0d6eb50d594f605
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-11-03 17:48:58 +0100

    feat(projects): add optional description field to project schema

--- src/features/projects/schema.ts
+++ src/features/projects/schema.ts
@@ -3,7 +3,8 @@ import { z } from 'zod'
 export const createProjectSchema = z.object({
   name: z.string().trim().min(1, 'The Name is required'),
   workspaceId: z.string().trim().min(1, 'The Workspace is required'),
-  shortcut: z.string().trim()
+  shortcut: z.string().trim(),
+  description: z.string().trim().optional()
 })
 
 export type CreateProjectSchema = z.infer<typeof createProjectSchema>
