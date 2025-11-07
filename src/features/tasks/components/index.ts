d32b2c2f3d46a25515dda045c34babb9f27cb7cc
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-11-07 10:51:46 +0100

    feat(tasks): add centralized tasks component exports
    - Create centralized index file for tasks components
    - Export page, modal, filters, views, create, edit, info, and comments components
    - Improve module organization and import efficiency for tasks feature
    - Simplify component imports across the application

---
+++ src/features/tasks/components/index.ts
@@ -0,0 +1,21 @@
+// Page components
+export * from './page'
+
+// Other components
+export { ModalTaskWrapper } from './modal-task-wrapper'
+export { TaskFilters } from './task-filters'
+
+// Views
+export * from './views'
+
+// Create
+export * from './create'
+
+// Edit
+export * from './edit'
+
+// Info
+export * from './info'
+
+// Comments
+export * from './comments'
