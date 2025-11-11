6c61a3c0fcd580626b4e2c83da0299643a2b6cef
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-11-11 22:44:32 +0100

    fix(layout): add z-index to navbar to ensure proper layering

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -36,7 +36,7 @@ export const Navbar = () => {
 
   // RENDER
   return (
-    <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 w-full sticky top-0 bg-background border-b'>
+    <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 w-full sticky top-0 bg-background border-b z-50'>
       <div className='flex items-center gap-2 px-4 w-full justify-between'>
         <div className='flex items-center gap-2'>
           <SidebarTrigger className='-ml-1' />
